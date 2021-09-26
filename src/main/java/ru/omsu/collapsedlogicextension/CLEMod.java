9d5b31f804171606a5545a1a78697a18a00e7f2c
Chae4ek <nominas.alexsey@gmail.com>
2021-09-26 13:48:33 +0600

    Working .jar file

---
+++ src/main/java/ru/omsu/collapsedlogicextension/CLEMod.java
@@ -0,0 +1,9 @@
+package ru.omsu.collapsedlogicextension;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CLEMod.MOD_ID)
+public class CLEMod {
+
+    public static final String MOD_ID = "collapsedlogicextension";
+}
