1edbde318d6890cc763a3e4be32c7052cbe7dc6a
Chae4ek <nominas.alexsey@gmail.com>
2021-09-27 18:28:33 +0600

    Add textures

--- src/main/java/ru/omsu/collapsedlogicextension/blocks/CLEBlockEnum.java
+++ src/main/java/ru/omsu/collapsedlogicextension/blocks/CLEBlockEnum.java
@@ -7,7 +7,9 @@ import ru.omsu.collapsedlogicextension.blocks.collapsedlogic.CollapsedLogicBlock
 public enum CLEBlockEnum {
     COLLAPSED_LOGIC_BLOCK("collapsed_logic_block", CollapsedLogicBlock::new);
 
+    /** Имя должно совпадать со всеми файлами ресурсов */
     private final String registryName;
+    /** При каждом вызове должен возвращать новый экземпляр блока */
     private final Supplier<CLEBlock> constructor;
 
     CLEBlockEnum(final String registryName, final Supplier<CLEBlock> constructor) {
@@ -15,10 +17,12 @@ public enum CLEBlockEnum {
         this.constructor = constructor;
     }
 
+    /** @return Регистрируемое имя блока */
     public String getRegistryName() {
         return registryName;
     }
 
+    /** @return Конструктор блока */
     public Supplier<CLEBlock> getConstructor() {
         return constructor;
     }
